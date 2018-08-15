9906283f530c72859d2ff472191828cb4ca7cd8f
HDD_REPO <trytocatch.arg@gmail.com>
2018-08-15 22:23:22 +0200

    add comment to link

--- mongoDB.py
+++ mongoDB.py
@@ -3,6 +3,7 @@ from pymongo import MongoClient
 
 def save_data(wargs):
     # client = MongoClient('mongodb://localhost:27017/')
+    # https://mlab.com/databases/mongo_bd#users
     client = MongoClient('mongodb://userPython:userPython123@ds121382.mlab.com:21382/mongo_bd')
     db = client.mongo_bd
     values_collection = db.values
